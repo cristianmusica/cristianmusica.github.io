df59a3df15704f87267f0e337152c9198c5e770a
cristianmusica <75284701+cristianmusica@users.noreply.github.com>
2023-12-02 02:04:03 -0300

    Update main.php

--- main.php
+++ main.php
@@ -1,47 +1,3 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Configuración de las credenciales
-    $api_url = "https://api-pasarela-sandbox.mcdesaqa.cl/payment-gateway/v1/orders";
-    $api_key = "mKaTZ4yBm3rVFapqNctziKCvXsjD6fDO";
-
-    // Datos de prueba desde el formulario
-    $monto = $_POST["monto"];
-    $tarjeta = $_POST["tarjeta"];
-
-    // Configurar datos para la solicitud
-    $data = array(
-        'monto' => $monto,
-        'tarjeta' => $tarjeta,
-        // Agregar otros datos necesarios para la transacción
-    );
-
-    // Configurar la solicitud HTTP con cURL
-    $ch = curl_init($api_url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Content-Type: application/json',
-        "Apikey: $api_key"
-    ));
-
-    // Ejecutar la solicitud cURL y obtener la respuesta
-    $response = curl_exec($ch);
-
-    // Manejar la respuesta (mostrar en este ejemplo)
-    if ($response === false) {
-        echo "Error de cURL: " . curl_error($ch);
-    } else {
-        $decoded_response = json_decode($response, true);
-        echo "<p>Respuesta de la API:</p>";
-        echo "<pre>" . print_r($decoded_response, true) . "</pre>";
-    }
-
-    // Cerrar la sesión cURL
-    curl_close($ch);
-}
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -51,6 +7,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <h1>Página de Prueba</h1>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $api_url = "https://api-pasarela-sandbox.mcdesaqa.cl/payment-gateway/v1/orders";
+        $api_key = "mKaTZ4yBm3rVFapqNctziKCvXsjD6fDO";
+
+        $monto = $_POST["monto"];
+        $tarjeta = $_POST["tarjeta"];
+
+        $data = array(
+            'monto' => $monto,
+            'tarjeta' => $tarjeta,
+            // Agregar otros datos necesarios para la transacción
+        );
+
+        $headers = array(
+            "Content-Type: application/json",
+            "Apikey: $api_key"
+        );
+
+        $ch = curl_init($api_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $response = curl_exec($ch);
+
+        if ($response === false) {
+            echo "Error de cURL: " . curl_error($ch);
+        } else {
+            $decoded_response = json_decode($response, true);
+            echo "<p>Respuesta de la API:</p>";
+            echo "<pre>" . print_r($decoded_response, true) . "</pre>";
+        }
+
+        curl_close($ch);
+    }
+    ?>
 
     <form method="post" action="">
         <label for="monto">Monto:</label>
